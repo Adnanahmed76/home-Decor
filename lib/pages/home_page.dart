306ad7068e433a1631db51205de9035cd96c9bb3
adnan ahmed <adnanahmed504423@gmail.com>
2025-07-23 18:37:39 +0530

    removed uncessarry files

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:homedecor/utilis/color.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     backgroundColor: DarkbackgroundColor,
+     body: Column(
+      children: [
+        
+      ],
+     ),
+    );
+  }
+}
